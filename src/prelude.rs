bcdbbb6809ee42b0eb640cb913da5b035e300383
feame <71621797+vicimeans@users.noreply.github.com>
2022-04-30 15:24:21 +0800

    prelude3

--- src/prelude.rs
+++ src/prelude.rs
@@ -1,2 +1,5 @@
 #[allow(unused_imports)]
+use crate::css_model::*;
+#[allow(unused_imports)]
+use crate::theme_center::*;
 pub use crate::create_theme_center_json::{create_render_metatable_json,output_to_render_metatable};
